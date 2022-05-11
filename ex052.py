449fb30a5b0a7df9c0620a5b0e74862e2cd35e5b
Leafarsf <rafael.sfernandes97@gmail.com>
2022-05-11 14:20:21 -0300

    correção

--- ex052.py
+++ ex052.py
@@ -1,10 +1,11 @@
 n = int(input("Digite um número e descubra se ele é um número primo ou não: "))
-primo = False
-for i in range (2,n):
-    if n % 1 == 0 and n % n == 0:
-        primo = True
-        break
-if primo == True:
+tot = 0
+for i in range (1,n + 1):
+    if n % i == 0:
+        tot +=1
+print(f'O número {n} foi divisível {tot} vezes')
+if tot <= 2:
     print(f"O número {n} é um número primo!")
 else:
     print(f"O número {n} não é um número primo!")
+
