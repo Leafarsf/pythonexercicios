b24fe12f009836236780047f37b3f509d886bc4f
Leafarsf <rafael.sfernandes97@gmail.com>
2022-05-17 17:47:04 -0300

    Update ex062.py

--- ex062.py
+++ ex062.py
@@ -6,13 +6,15 @@ razao = int(input("Insira a razão de sua P.A: "))
 n_termos = int(input("Insira o número de termos que você irá visualizar: "))
 termo = primeiro_termo
 contador = 0
+termos = 0
 while n_termos > 0:
-    while contador <= n_termos:
+    while contador < n_termos:
         print(f"{termo} > ", end='')
         termo = termo + razao
         contador += 1
+        termos += 1
     print("FIM DA P.A")
     n_termos = int(input("Caso queira continuar, insira o número de termos: "))
     contador = 0
-print("FIM")
+print(f"A P.A foi encerrada após {termos} termos")
 
