4eb5ed13d96c9068fa072ff5cfcdd9c995fc8c7d
Leafarsf <rafael.sfernandes97@gmail.com>
2022-05-17 13:52:26 -0300

    Update ex055.py

--- ex055.py
+++ ex055.py
@@ -1,6 +1,6 @@
 peso = []
-for i in range(0,5):
-    kg = float(input("Insira o peso da pessoa em Kg: "))
+for i in range(1,6):
+    kg = float(input(f"Insira o peso da {i} pessoa em Kg: "))
     peso.append(kg)
 peso = [float(i) for i in peso]
 print(f"O menor peso é {min(peso)} e o maior peso é {max(peso)}")
